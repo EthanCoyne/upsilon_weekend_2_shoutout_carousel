0ccb8977683350a888477f67d260fa6008d1e854
Ethan Coyne <ethancoyne@Ethans-MacBook-Pro.local>
2017-01-15 16:23:19 -0600

    added timer functionality

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -29,11 +29,21 @@ $(document).ready(function(){
 
       $('#next').on('click', function(){
         nextButton();
+        $('.highlighted').removeClass('highlighted');
+        $(this).closest('body').find('#' + i).addClass('highlighted');
       });
 
       $('#prev').on('click', function(){
         prevButton();
+        $('.highlighted').removeClass('highlighted');
+        $(this).closest('body').find('#' + i).addClass('highlighted');
       });
+
+      window.setInterval(function(){
+        nextButton();
+        $('.highlighted').removeClass('highlighted');
+        $('#person').closest('body').find('#' + i).addClass('highlighted');
+      }, 10000);
 
 
 
@@ -56,44 +66,36 @@ $(document).ready(function(){
 
       function nextButton() {
         if(i < 16 ){
-          $('.highlighted').removeClass('highlighted');
           i = next;
           var person = data.people[i];
           appendDom(person);
           console.log(i);
           next = i + 1;
           prev = i - 1;
-          $(this).closest('body').find('#' + i).addClass('highlighted');
         } else {
-          $('.highlighted').removeClass('highlighted');
           i = 0;
           var person = data.people[i];
           appendDom(person);
           next = i + 1;
           prev = i - 1;
-          $(this).closest('body').find('#' + i).addClass('highlighted');
         }
       };
 
       function prevButton() {
         if(i > 0) {
-          $('.highlighted').removeClass('highlighted');
           i = prev;
           var person = data.people[i];
           appendDom(person);
           console.log(i);
           next = i + 1;
           prev = i - 1;
-          $(this).closest('body').find('#' + i).addClass('highlighted');
         } else {
-          $('.highlighted').removeClass('highlighted');
           i = 16;
           var person = data.people[i];
           appendDom(person);
           console.log(i);
           next = i + 1;
           prev = i - 1;
-          $(this).closest('body').find('#' + i).addClass('highlighted');
         }
       };
     }
